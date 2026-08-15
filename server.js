69a93e374e0f467ca541add7fe5bd217821500fc
shifa <shifa@Shifa.localdomain>
2026-08-15 13:42:27 +0000

    Add about route in feature branch

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-const env = process.argv[2] || "development";
-dotenv.config({ path: `.env.${env}`});
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const APP = process.env.APP || 'Development';
-
-app.get('/', (req, res) => {
-    res.send(`Hello world from express from ${APP}`);
+app.get('/about',(req,res) => {
+	res.send('Hello from about Page - Feature Branch');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} in ${APP} mode`);
-});
