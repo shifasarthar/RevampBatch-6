1b8c8c60f8c4e2a3b8b653a438329dac63fa6cd2
shifa <shifa@Shifa.localdomain>
2026-08-15 13:47:25 +0000

    Revert "Add about route in feature branch"
    This reverts commit 69a93e374e0f467ca541add7fe5bd217821500fc.

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-app.get('/about',(req,res) => {
-	res.send('Hello from about Page - Feature Branch');
+const express = require('express');
+const dotenv = require('dotenv');
+
+const env = process.argv[2] || "development";
+dotenv.config({ path: `.env.${env}`});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const APP = process.env.APP || 'Development';
+
+app.get('/', (req, res) => {
+    res.send(`Hello world from express from ${APP}`);
 });
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} in ${APP} mode`);
+});
