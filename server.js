b5760a8036d21a398c1110eed3eadcb9a67b7c7f
shifa <shifa@Shifa.localdomain>
2026-08-16 08:16:41 +0000

    Add contact route

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-const env = process.argv[2] || "development";
-dotenv.config({ path: `.env.${env}`});
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const APP = process.env.APP || 'Development';
-
-app.get('/', (req, res) => {
-    res.send(`Hello world from express from ${APP}`);
+app.get('/contact',(req,res) => {
+	res.send('Contact Page');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} in ${APP} mode`);
-});
